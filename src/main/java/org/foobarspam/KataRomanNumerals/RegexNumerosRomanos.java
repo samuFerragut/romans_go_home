caf54daba70112430718bf4bd520edcf4bea99c6
samuFerragut <sferragut@cifpfbmoll.eu>
2020-09-16 16:26:35 +0200

    feat(RegexNumerosRomanos, NumeroRomano): Crear clase RegexNumerosRomanos y explotar el Map

---
+++ src/main/java/org/foobarspam/KataRomanNumerals/RegexNumerosRomanos.java
@@ -0,0 +1,32 @@
+package org.foobarspam.KataRomanNumerals;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+public class RegexNumerosRomanos {
+
+    private Map<String, String> regexDiccionario = new HashMap<String, String>();
+
+    public void addRegex(String key, String value) {
+        this.regexDiccionario.put(key, value);
+    }
+
+    /** Explotamos el Map */
+
+    public Map<String, String> getRegex() {
+        return this.regexDiccionario;
+    }
+
+    public String getRegexValue(String key) {
+        return this.regexDiccionario.get(key);
+    }
+
+    public Collection<String> getValues() {
+        return this.regexDiccionario.values();
+    }
+
+    public long longitud() {
+        return this.regexDiccionario.size();
+    }
+}
